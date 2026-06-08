0f4fac6917845faa590d89f20eb08506d0c0914f
0445968 <crafterkite@gmail.com>
2026-06-08 00:28:52 -0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -152,8 +152,7 @@ export default {
 
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
-        serif: ['Newsreader', 'Georgia', 'serif'],
-        display: ['Newsreader', 'Georgia', 'serif'],
+        display: ['Plus Jakarta Sans', 'Inter', 'system-ui', 'sans-serif'],
       },
 
       boxShadow: {
